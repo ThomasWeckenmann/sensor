de2675f05f46ee182cf3711e30e1d19620d4cad4
ThomasWeckenmann <tweckenmann0711@gmail.com>
2022-10-01 14:27:25 +0200

    Add files via upload

---
+++ sensor.py
@@ -0,0 +1,21 @@
+import numpy as np
+import altair as alt
+import requests
+import pandas as pd
+import streamlit as st
+import datetime
+from dateutil.parser import parse
+from dateutil.relativedelta import *
+
+osm_url = "https://api.opensensemap.org/boxes/5e92b3e9df8258001bdfc8eb"
+rohr_data_osm = requests.get(osm_url).json()
+sensors = rohr_data_osm["sensors"]
+sensor_date = parse(sensors[0]["lastMeasurement"]["createdAt"]) + relativedelta(hour=2)
+required_date = sensor_date.strftime("%a %d %b %Y, %H:%M:%S GMT")
+
+
+st.header(rohr_data_osm["name"])
+for sensor in sensors:
+    st.write(sensor["lastMeasurement"]["value"], sensor["unit"], sensor["title"])
+    
+st.write(sensor_date)
