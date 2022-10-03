721864f85885d21dd753f9fbe47a5bae8374b4f8
ThomasWeckenmann <tweckenmann0711@gmail.com>
2022-10-03 16:47:29 +0200

    Add High Values Slider

--- sensor.py
+++ sensor.py
@@ -7,7 +7,7 @@ from dateutil.parser import parse
 import altair as alt
 from dateutil.relativedelta import *
 
-high_value = 30
+
 box_id = "5e92b3e9df8258001bdfc8eb"
 osm_url = f"https://api.opensensemap.org/boxes/{box_id}"
 osm_url1000 = "https://api.opensensemap.org/boxes/{box_id}/data/{sensor_id}?&download=true&format=json"
@@ -25,20 +25,12 @@ for sensor in sensors:
     sensor_data1000 = requests.get(url1000).json()
     values = []
     dates = []
-    if sensor["title"] == "PM10":
-        pm10_values_high = {}
-    if sensor["title"] == "PM2.5":
-        pm25_values_high= {}
     for sensor_data in sensor_data1000:
         value = float(sensor_data["value"])
         values.append(value)
         value_date = parse(sensor_data["createdAt"]) + timedelta(hours=2)
         date = value_date.strftime('%d.%m.%y %H:%M:%S')
         dates.append(date)
-        if sensor["title"] == "PM10" and value >= high_value:
-            pm10_values_high[date] = value
-        if sensor["title"] == "PM2.5" and value >= high_value:
-            pm25_values_high[date] = value
     try:
         df[sensor["title"].replace(".", "")] = values
         df["date"] = dates
@@ -46,9 +38,17 @@ for sensor in sensors:
         pass
 st.write("gemessen um ", sensor_date.strftime('%H:%M:%S'), "Uhr")
 
+high_value = st.slider('Show Values over', 10, 100, 40)
+
+df_high_10 = df[df.PM10 > high_value]
+df_high_25 = df[df.PM25 > high_value]
+merged_df_high = pd.concat([df_high_10, df_high_25], ignore_index = True, sort = False)
+merged_df_high = merged_df_high[['date'] + [x for x in merged_df_high.columns if x != 'date']]
+
+st.write("Values over ", high_value, merged_df_high)
+
+st.header("Charts:")
 pm10 = alt.Chart(df).mark_line().encode(x='date',y='PM10')
 pm025 = alt.Chart(df).mark_line().encode(x='date',y='PM25')
-st.write("PM2.5 Values over 40:", pm25_values_high)
-st.write("PM10 Values over 40:", pm10_values_high)
 st.altair_chart(pm10)
 st.altair_chart(pm025)
