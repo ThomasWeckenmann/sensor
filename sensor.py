65632698752366fd6c5ccba306471c5ab0f28b58
ThomasWeckenmann <tweckenmann0711@gmail.com>
2022-10-05 08:58:16 +0200

    Add sidebar and use st line chart instead of Altair

--- sensor.py
+++ sensor.py
@@ -4,11 +4,16 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta
 from dateutil.parser import parse
-import altair as alt
 from dateutil.relativedelta import *
 from geopy.geocoders import Nominatim
 
-box_ids = ["5e92b3e9df8258001bdfc8eb", "5c377effc4c2f3001942a946", "5d9ef41e25683a001ad916c3"]
+box_ids = [
+    "5e92b3e9df8258001bdfc8eb", 
+    "5c377effc4c2f3001942a946", 
+    "5d9ef41e25683a001ad916c3",
+    "5b37aae81fef04001b41961f",
+    "5e941c07df8258001b663b40"
+]
 
 class Box(object):
     def __init__(self, box_id):
@@ -58,13 +63,14 @@ class Box(object):
 
 
 def user_box_id_selection():
+    
     box_id_dict = {}
     for box_id in box_ids:
         box = Box(box_id)
         combo_key = box.box_data["name"] + box.address_string
         box_id_dict[combo_key] = box_id
-    combo_id_combo = st.selectbox('Select a Box:',(box_id_dict))
-    box_manual_input = st.text_input("Or paste a Box ID:", "")
+    combo_id_combo = st.sidebar.selectbox('Select a Box:',(box_id_dict))
+    box_manual_input = st.sidebar.text_input("Or paste a Box ID:", "")
     if box_manual_input:
         box_id = box_manual_input
     else:
@@ -82,12 +88,7 @@ def show_high_pm_values(box):
     
 
 def show_pm_graphs(box):
-    pm10 = alt.Chart(box.df).mark_line().encode(x='date',y='PM10')
-    pm025 = alt.Chart(box.df).mark_line().encode(x='date',y='PM25')
-    st.write("PM10 (last 1000 measurements)")
-    st.altair_chart(pm10)
-    st.write("PM2.5 (last 1000 measurements)")
-    st.altair_chart(pm025)
+    st.line_chart(box.df, x='date',y=['PM10', "PM25"], width=200, height=400)
 
 
 def run(box_id):
